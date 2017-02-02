09069ac93e8965df744d69e37767f06e3ad6f965
Anze Slosar <slosar@gmail.com>
2017-02-02 11:38:31 -0500

    try2

--- py/makehp.py
+++ py/makehp.py
@@ -7,17 +7,23 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-Nside=2048
+Nside=256
 Npix=12*Nside**2
 
-def filterAvg(s):
-    sr=s*1.0
-    Nx,Ny=s.shape
-    for i in range(Nx):
-        sr[i,:]-=sr[i,:].mean()
-    for j in range(Ny):
-        sr[:,j]-=sr[:,j].mean()
-    return sr
+def filterAvg(sky):
+    tsky=np.hstack(sky).mean(axis=1)
+    fsky, fwe=[],[]
+    for s in sky:
+        Nx,Ny=s.shape
+        fwe.append(1.0/s)
+        scaling=(s.mean(axis=1)/tsky).mean()
+        print scaling
+        fs=s*1.0-np.outer(scaling*tsky,np.ones(256))
+        for i in range(Ny):
+            fs[:,i]-=fs[:,i].mean()
+        fsky.append(fs)
+
+    return fsky, fwe
         
 
 def main():
@@ -33,18 +39,34 @@ def main():
     print flist
     for i,fname in enumerate(flist):
         print "Processing ",fname, i+1,"/",len(flist)
-        header,sky,ra,dec=np.load(fname)['arr_0']
-        sky=filterAvg(sky)
-        sky=sky.flatten()
-        ra=ra.flatten()
-        dec=dec.flatten()
-        theta=np.pi/2-dec/180.*np.pi
-        phi=ra/180.*np.pi
-        ipix=hp.ang2pix(Nside,theta,phi)
-        wmap+=np.bincount(ipix,minlength=Npix)
-        gmap+=np.bincount(ipix,weights=sky,minlength=Npix)
-        vmap[ipix]+=1
-    gmap/=wmap
+        if "-1-" not in fname:
+            continue
+        headerl,skyl,ral,decl=[],[],[],[]
+        for i in range(6):
+            print i,
+            cf=fname.replace('-1-','-%i-'%(i+1))
+            cheader,csky,cra,cdec=np.load(cf)['arr_0']
+            headerl.append(cheader)
+            skyl.append(csky)
+            ral.append(cra)
+            decl.append(cdec)
+        print
+        skyl,weil=filterAvg(skyl)
+        for sky,wei,ra,dec in zip(skyl,weil,ral,decl):
+            sky=sky.flatten()
+            ra=ra.flatten()
+            dec=dec.flatten()
+            wei=wei.flatten()
+            theta=np.pi/2-dec/180.*np.pi
+            phi=ra/180.*np.pi
+            ipix=hp.ang2pix(Nside,theta,phi)
+            wmap+=np.bincount(ipix,weights=wei,minlength=Npix)
+            gmap+=np.bincount(ipix,weights=sky*wei,minlength=Npix)
+            vmap[ipix]+=1
+    assert(np.all(gmap[np.where(wmap==0)]==0))
+    assert(len(np.where(wmap<0)[0])==0)
+    mx=np.where(wmap>0)
+    gmap[mx]/=wmap[mx]
     np.savez("map",(gmap, wmap, vmap))
     # plt.figure(figsize=(12,8))
     # hp.mollview(wmap)
